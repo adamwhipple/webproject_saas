9d8bb28c849b78908977548d966420edcfc404eb
Adam Whipple <adamwhipple1@gmail.com>
2015-07-05 21:48:34 +0000

    Fixed typo

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,11 +1,10 @@
 class ContactMailer < ActionMailer::Base
     default to: 'adamwhipple1@gmail.com'
 
-
     def contact_email(name, email, body)
         @name = name
         @email = email
-        @body = mody
+        @body = body
         
         mail(from: email, subject: 'Contact Form Massage')
     end
