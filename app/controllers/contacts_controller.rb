ae7579e70d6ae46e19530c827c2f27548bbb6bc4
Adam Whipple <adamwhipple1@gmail.com>
2015-07-05 22:02:14 +0000

    Still fixing errors

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -20,7 +20,7 @@ class ContactsController < ApplicationController
             redirect_to new_contact_path
         end
     end
-    
+
     private
         def contact_params
             params.require(:contact).permit(:name, :email, :comments)
